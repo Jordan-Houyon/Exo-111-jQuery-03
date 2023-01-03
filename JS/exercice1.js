ab861c91a65fef7daba6266aac8c33666135406e
arthu <jordnhouyon@outlook.fr>
2023-01-03 09:38:40 +0100

    exercice finished

---
+++ JS/exercice1.js
@@ -0,0 +1,10 @@
+// ------- 1 ------- //
+
+let math =$("#math-heading");
+console.log(math);
+math.append("... Congratulations !!!!!!");
+
+let science =$("#science-heading");
+console.log(science);
+science.append("It's amazing !!");
+
